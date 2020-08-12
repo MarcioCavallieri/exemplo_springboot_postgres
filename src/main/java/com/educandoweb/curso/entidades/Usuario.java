f70bf5961697fb4677343a22515b42ca1a80c57e
MarcioCavallieri <mvccavallieri@gmail.com>
2020-08-12 08:42:46 -0300

    * Atualização do pow.xml para baixar o banco h2 para teste * Criação do arquivo 'application-test.properties', com configurações do banco de teste em memória * Mapeamento da classe 'Usuario' com as notações do Hibernate

--- src/main/java/com/educandoweb/curso/entidades/Usuario.java
+++ src/main/java/com/educandoweb/curso/entidades/Usuario.java
@@ -1,9 +1,17 @@
 package com.educandoweb.curso.entidades;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-public class Usuario implements Serializable {	
+@Entity
+public class Usuario implements Serializable {		
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
@@ -15,7 +23,6 @@ public class Usuario implements Serializable {
 	}
 	
 	public Usuario(Long id, String nome, String email, String telefone, String senha) {
-		super();
 		this.id = id;
 		this.nome = nome;
 		this.email = email;
