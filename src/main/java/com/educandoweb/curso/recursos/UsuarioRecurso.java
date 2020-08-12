8e881d71b5446023d4490c379fa7dfbdf1020213
MarcioCavallieri <mvccavallieri@gmail.com>
2020-08-12 08:20:21 -0300

    Criadas as classes Usuario e UsuarioRecurso

---
+++ src/main/java/com/educandoweb/curso/recursos/UsuarioRecurso.java
@@ -0,0 +1,19 @@
+package com.educandoweb.curso.recursos;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.curso.entidades.Usuario;
+
+@RestController
+@RequestMapping(value = "/usuarios")
+public class UsuarioRecurso {
+	
+	@GetMapping
+	public ResponseEntity<Usuario> findAll() {
+		Usuario u = new Usuario(1L, "Marcio", "Marcio@Marcio", "123456789", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
